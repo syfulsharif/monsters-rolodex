cb82b083bc743e9c19b655ffcad3b7dd7101bff9
syfulsharif <sharif.syful@gmail.com>
2022-09-30 22:48:17 +0600

    project updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super();
+
+    this.state = {
+      monsters : [],
+      searchField : ''
+    }
+  }
+
+  componentDidMount () {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then( response => response.json())
+      .then( users => this.setState(
+        () => {
+          return {monsters: users};
+        },
+        () => {
+          console.log(this.state);
+        }
+      ) )
+  }
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    })
+  }
+
+
   render () {
+    
+    const { monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+
+    });
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello, This is Sharif, Learning React
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input 
+          className="search-box" 
+          type="search" 
+          placeholder='Search Monsters' 
+          onChange={onSearchChange}/>
+        {filteredMonsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1>{monster.name}</h1>
+              </div>
+            );            
+          })
+        }
       </div>
     );
   }
